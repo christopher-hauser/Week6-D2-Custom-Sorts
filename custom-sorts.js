6804d640bd22e8ca9e251da6c33e2bb29cda90df
Christopher Hauser <chauser6@gmail.com>
2021-09-28 13:10:29 -0400

    3 of 5 solved

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,23 +1,50 @@
-
-
-function ageSort(users) {
-  // Fill this in
+function quicksort(arr) {
+  if (arr.length <= 1) return arr;
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+  let leftSort = quicksort(left);
+  let rightSort = quicksort(right);
+  return [...leftSort, pivot, ...rightSort];
 }
 
-
 function oddEvenSort(arr) {
-  // Fill this in
+  let odds = [];
+  let evens = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 1) odds.push(arr[i]);
+    else evens.push(arr[i]);
+  }
+
+  sortOdds = quicksort(odds);
+  sortEvens = quicksort(evens);
+  return [...sortOdds, ...sortEvens];
 }
 
 function validAnagrams(s, t) {
-  // Fill this in
+  if (quicksort(s).join("") === quicksort(t).join("")) return true;
+  return false;
 }
 
 function reverseBaseSort(arr) {
-  // Fill this in
+  return arr.sort((a, b) => {
+    let baseA = Math.floor(Math.log10(a));
+    let baseB = Math.floor(Math.log10(b));
+    if (baseA === baseB) return a - b;
+    if (baseA > baseB) return -1;
+    else return 1;
+  })
 }
 
 function frequencySort(arr) {
+  
+}
+
+function ageSort(users) {
   // Fill this in
 }
 
