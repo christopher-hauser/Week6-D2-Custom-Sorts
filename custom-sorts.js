356d4f2052564fbc9312f8d53840b14e9b69bc5d
Christopher Hauser <chauser6@gmail.com>
2021-09-28 12:24:03 -0400

    first commit

---
+++ custom-sorts.js
@@ -0,0 +1,25 @@
+
+
+function ageSort(users) {
+  // Fill this in
+}
+
+
+function oddEvenSort(arr) {
+  // Fill this in
+}
+
+function validAnagrams(s, t) {
+  // Fill this in
+}
+
+function reverseBaseSort(arr) {
+  // Fill this in
+}
+
+function frequencySort(arr) {
+  // Fill this in
+}
+
+module.exports = [oddEvenSort, validAnagrams, reverseBaseSort,
+                  frequencySort, ageSort];
